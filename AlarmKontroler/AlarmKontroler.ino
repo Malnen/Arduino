b13c44046e13fcd6ba0237c29109cf10a720a435
Marcel Niedziela <46851853+Malnen@users.noreply.github.com>
2020-05-14 14:59:07 +0200

    Add files via upload

--- AlarmKontroler/AlarmKontroler.ino
+++ AlarmKontroler/AlarmKontroler.ino
@@ -1,30 +1,29 @@
 #include <IRAlarm.h>
 
-IRAlarm alarm(1);
+IRAlarmWithTamper alarm(1);
 
 int pinNotification = 3;
 int pinAlarm = 2;
 void setup() {
-  alarm.addSensor(new IRSensor(4));
-  alarm.addSensor(new IRSensor(5));
-  alarm.addSensor(new IRSensor(6));
+  alarm.addSensor(new IRSensorWithTamper(5,6));
   pinMode(pinAlarm, OUTPUT);
   pinMode(pinNotification, INPUT);
-  //Serial.begin(9600);
-
+  pinMode(LED_BUILTIN, OUTPUT);
+ // Serial.begin(9600);
 }
 
 void loop() {
   alarm.execute();
-
+ 
   if (digitalRead(pinNotification)) {
     if (alarm.alarm) {
       digitalWrite(pinAlarm, HIGH);
+      digitalWrite(LED_BUILTIN, HIGH);
     }
   } else {
     alarm.alarm = false;
       digitalWrite(pinAlarm, LOW);
+      digitalWrite(LED_BUILTIN, LOW);
   }
- // Serial.println("Alarm: "+ (String)alarm.alarm);
- // delay(1000);
+ //Serial.println("Alarm: "+ (String)alarm.alarm);
 }
